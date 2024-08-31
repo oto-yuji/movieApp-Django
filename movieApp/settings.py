c70057ab15fb689c9ab9794a554ebd336702fcf9
oto-yuji <yuji.oto@divx.co.jp>
2024-09-01 02:59:27 +0900

    AollowedHostにrenderをついか

--- movieApp/settings.py
+++ movieApp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
 
 # Application definition
