8be50e95c0d269a30a9192e4af3ad01105a01ca3
oto-yuji <yuji.oto@divx.co.jp>
2024-09-01 03:24:58 +0900

    一時的にデバッグON

--- movieApp/settings.py
+++ movieApp/settings.py
@@ -26,7 +26,7 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG', default=False)
+DEBUG = env('DEBUG', default=True)
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
