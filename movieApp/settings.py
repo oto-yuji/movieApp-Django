3a263c18d0ac6a90a7113157a06ece0cc716b2cd
oto-yuji <yuji.oto@divx.co.jp>
2024-09-02 20:02:31 +0900

    ALLOWED HOSTSのパスをフルに

--- movieApp/settings.py
+++ movieApp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=True)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django-render-1ra5.onrender.com']
 
 
 # Application definition
